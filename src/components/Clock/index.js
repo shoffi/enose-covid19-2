3e8f39f9bc937ab9d45daf27648852f5fb1dbcb0
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2020-10-05 13:44:20 +0700

    add clock

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 
 class Clock extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            dateClass: new Date()
+        }
+
+        this.time = this.state.dateClass.toLocaleTimeString();
+        this.hourMin = this.time.length === 10? this.time.slice(0) : this.time.slice(0,5);
+    }
+
+    setTime = () => {
+        this.setState({
+            dateClass: new Date()
+        })
+        this.time = this.state.dateClass.toLocaleTimeString();
+        this.hourMin = this.time.length === 10? this.time.slice(0) : this.time.slice(0,5);
+    }
+
+    componentDidMount () {
+        setInterval(this.setTime, 1000)
+    }
+    
     render () {
         return (
             <div>
-                Clock
+                {this.hourMin}
             </div>
         )
     }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,7 @@ class Nav extends Component {
     render () {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Rumah Sakit Dr Soetomo</a>
+                <p className="navbar-brand">Rumah Sakit Dr Soetomo</p>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
