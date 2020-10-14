04ad1224d26c8504706a9e8b79f0ed7554dbd3ea
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2020-10-14 18:03:06 +0700

    :update: add symptomps and comorbisities

--- src/components/Pages/AmbilSample.js
+++ src/components/Pages/AmbilSample.js
@@ -28,16 +28,118 @@ class AmbilSample extends Component {
             <>
                 <button style={{width:'100%'}} className="btn btn-warning mt-5 mb-5" onClick={() => this.setState({redirect: '/menu'})}>Kembali</button>
                 <div className="mt-5">
-                    <div className="text-center col-md-4 ml-auto mr-auto">hxz
+                    <div className="col-md-8 ml-auto mr-auto">
                         <form onSubmit={this.handleSubmit}>
+                            
                             <div className="form-group">
                                 <label>ID Pasien:</label>
                                 <input className="form-control" type="text" value={this.props.patientId} onChange={this.props.setPatientId} />
                             </div>
+
                             <div className="form-group">
-                                <input type="checkbox" aria-label="Checkbox for following text input" />
+                                <input type="checkbox" />
                                 <label className="ml-2">Negatif Covid-19</label>
                             </div>
+
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Fever</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Flu/Sneeze</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Sore Throat</label>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-4">
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Cough</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Nausea</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Diarrhea</label>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-4">
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Headache</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Watery eyes</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Diff breath</label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Hypertension</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Diabetes Mellitus</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Cardiovasular disease</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Heart disease</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Kidney disease</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Respiratory disease</label>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Astma</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Cancer</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Tuberkolusis</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Liver disease</label>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="checkbox" />
+                                        <label className="ml-2">Immune disorders</label>
+                                    </div>
+                                </div>
+                            </div>
+
                             <input type="submit" value="Start Sampling" />
                         </form>
                     </div>
