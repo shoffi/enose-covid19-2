2d21d50da25117bbb3041744e4b18e2e57e2dce5
sofiizza <shoffi.izza@gmail.com>
2020-11-14 00:08:18 +0700

    Update Welcome page

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ class App extends Component {
         <div className="min-h-screen flex flex-col">
           <TopNav></TopNav>
           <Router>
-            <div className="flex-grow">
+            <div className="flex-grow mx-auto">
             
               <Route path='/' exact>
                   <Welcome
--- src/components/Pages/Welcome.js
+++ src/components/Pages/Welcome.js
@@ -23,31 +23,27 @@ class Welcome extends Component {
         }
 
         return (
-            <>
-                <h3 className="mt-5 text-center text-bold" >Welcome to Electronic Nose ITS</h3>
-                <div className="mt-3 text-center">
-                    <img 
-                        src={logo}
-                        style={{
-                            width:'300px'
-                        }}
-                        className="mt-5"
-                        alt='Logo Enosika'
-                    />
+            <div className="flex mt-20">
+                <div className="w-64">
+                    <img src={logo} className="" alt="Logo I-Nose" />
                 </div>
-                <div className="text-center">
-                    <button 
-                        style={{
-                            borderRadius:'20px',
-                        }}
-                        className="btn btn-success mt-5 px-5"
-                        fontWeight="bold" 
-                        onClick={()=>this.handleMasuk()}
-                    >
-                        Power On
+                <div>
+                    <div className="text-4xl leading-tight mb-8">
+                        <h2 className="font-light">Welcome to</h2>
+                        <h2 className="font-bold">Electronic Nose ITS</h2>
+                    </div>
+                    <button
+                    onClick={()=>this.handleMasuk()}
+                    className="flex items-center justify-center border-2 border-gray-900 w-full rounded-md text-lg font-semibold py-2 focus:outline-none">
+                        <svg className="w-8 h-8 text-gray-900 mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M7 6a7.75 7.75 0 1 0 10 0" />
+                            <line x1="12" y1="4" x2="12" y2="12" />
+                        </svg>
+                        Power on
                     </button>
                 </div>
-            </>
+            </div>
         );
     }
 }
