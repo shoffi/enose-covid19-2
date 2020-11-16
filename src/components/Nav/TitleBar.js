93fcb414064f6718e3c35f0f3e1726b959f5a5bd
sofiizza <shoffi.izza@gmail.com>
2020-11-17 01:15:30 +0700

    Change select method & add export button

--- src/components/Nav/TitleBar.js
+++ src/components/Nav/TitleBar.js
@@ -3,12 +3,12 @@ import React, { Component } from "react";
 class TitleBar extends Component {
     render () {
         return (
-            <div className="relative flex justify-between">
+            <div className="relative flex justify-between h-6">
                 <div className="z-10">
                     {this.props.back && (
                         <button
                         onClick={this.props.setBack}
-                        className="flex items-center text-lg w-full border-2 border-gray-500 text-gray-500 font-semibold focus:outline-none rounded-lg pr-3 pl-1 py-2">
+                        className="flex items-center text-lg w-full border-2 border-gray-500 text-gray-500 font-semibold focus:outline-none rounded-lg pr-3 pl-1 py-1">
                             <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
                             Kembali
                         </button>
@@ -21,7 +21,7 @@ class TitleBar extends Component {
                     {this.props.next && (
                         <button
                         onClick={this.props.setNext}
-                        className="flex justify-end items-center text-lg w-full border-2 border-green-600 text-green-600 font-semibold focus:outline-none rounded-lg pl-3 pr-1 py-2">
+                        className="flex justify-end items-center text-lg w-full border-2 border-green-600 text-green-600 font-semibold focus:outline-none rounded-lg pl-3 pr-1 py-1">
                             {this.props.setNextName}
                             <svg class="w-6 h-6 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
                         </button>
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -29,8 +29,8 @@ class Home extends Component {
         }
     }
 
-    openModal(e) {
-        e.preventDefault();
+    openModal() {
+        // e.preventDefault();
         this.setState(state => ({
             isModalOpen: !state.isModalOpen
         }));
@@ -41,6 +41,7 @@ class Home extends Component {
             selectedRuangan: kamar,
         })
         this.props.setRuangId(kamar);
+        this.openModal();
     }
 
     render() {
@@ -79,14 +80,14 @@ class Home extends Component {
                                 className="text-left flex-1 font-semibold px-4 py-2">
                                 {ruangan}
                                 </p>
-                                <svg class="w-8 h-8 text-gray-400 mx-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path></svg>
+                                <svg class="w-8 h-8 text-brand-orange mx-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M19 9l-7 7-7-7"></path></svg>
                             </button>
                         </div>
                     </div>
                     <button
                     onClick={this.handleSubmit}
                     disabled={!this.props.nurseId && !this.props.ruangId.id}
-                    className={`${this.props.nurseId && this.props.ruangId.id ? 'bg-brand-green' : 'bg-gray-400 cursor-not-allowed'} items-center justify-center w-48 h-48 text-white focus:outline-none rounded-md`}>
+                    className={`${this.props.nurseId && this.props.ruangId.id ? 'bg-brand-orange' : 'bg-gray-400 cursor-not-allowed'} items-center justify-center w-48 h-48 text-white focus:outline-none rounded-md`}>
                         <svg class="mx-auto w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"></path></svg>
                         <p className="text-xl font-semibold">Masuk</p>
                     </button>
@@ -96,24 +97,16 @@ class Home extends Component {
                 
                 {this.state.isModalOpen && (
                     <div className="absolute bg-white w-full pt-8">
-                    <div className="mx-auto w-2/3 bg-white border-4 border-gray-900 rounded-lg overflow-hidden">
+                    <div className="mx-auto w-2/3 bg-white border-4 border-brand-green rounded-lg overflow-hidden">
                         <div className="flex items-center p-3">
-                            <div className="flex-1 leading-tight">
-                                <p className="text-2xl font-semibold">Pilih Ruangan</p>
-                                <span className="text-gray-600">Scroll kebawah untuk memilih</span>
-                            </div>
-                            <div>
-                                <button
-                                onClick={this.openModal}
-                                className="bg-gray-900 px-10 py-2 text-xl text-white focus:outline-none rounded-md">Pilih</button>
-                            </div>
+                            <p className="text-2xl font-semibold text-brand-green">Pilih Ruangan</p>
                         </div>
-                        <ul className="bg-gray-200 divide-y-2 divide-gray-300 text-2xl text-gray-700">
+                        <ul className="bg-gray-100 divide-y-2 divide-gray-200 text-2xl text-gray-700">
                             {this.state.ruangan.map(kamar => (
                                 <li
                                     key={kamar.id}
                                     onClick={() => this.selectRuangan(kamar)}
-                                    className={`p-3 ${this.state.selectedRuangan === kamar ? "bg-gray-900 text-white" : "hover:bg-gray-300 hover:text-gray-900"} cursor-pointer`}>
+                                    className={`p-3 ${this.state.selectedRuangan === kamar ? "bg-brand-orange text-white" : "hover:bg-gray-200 hover:text-gray-900"} cursor-pointer`}>
                                     Ruangan {kamar.name}
                                 </li>
                             ))}
