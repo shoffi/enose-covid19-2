ae32a7f45598cc7f771152781d561fe838588f15
Azzam Jihad Ulhaq <azzam.jiul@gmail.com>
2020-12-17 21:02:13 +0700

    update Log Class

--- electron/Log.js
+++ electron/Log.js
@@ -6,7 +6,6 @@ class Log {
     constructor (opts) {
         const userDataPath = (electron.app || electron.remote.app).getPath('userData');
         this.path = path.join(userDataPath, opts.configName + '.json');
-        this.data = parseDataFile(this.path, opts.defaults);
     }
 
     get(key) {
@@ -18,8 +17,7 @@ class Log {
         fs.writeFileSync(this.path, JSON.stringify(this.data));
     }
 
-    insert  = async (json_data) => {
-        
+    insert  = (json_data) => {
         fs.readFile(this.path, 'utf8',(err, jsonString) => {
             console.log(jsonString)
             try {
@@ -32,17 +30,6 @@ class Log {
                 fs.writeFileSync(this.path, JSON.stringify(data));
             }
         })
-    }
-}
-
-function parseDataFile(filePath, defaults) {
-    // We'll try/catch it in case the file doesn't exist yet, which will be the case on the first application run.
-    // `fs.readFileSync` will return a JSON string which we then parse into a Javascript object
-    try {
-      return JSON.parse(fs.readFileSync(filePath));
-    } catch(error) {
-      // if there was some kind of error, return the passed in defaults instead.
-      return defaults;
     }
 }
   
